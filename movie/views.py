3c70bdc3c2c4907966594a6bb8544cdb7fe4ba14
Jeronimo Cardona <jerocardona312@gmail.com>
2024-02-08 15:37:57 -0500

    Cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page </h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'greg'})
-    
+    return render(request, 'home.html', {'name': 'Jero Cardona'})
